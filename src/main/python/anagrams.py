e6806bdf21df3d991d464beaa710f3a3712339e3
Alstonargodi <55945729+Alstonargodi@users.noreply.github.com>
2024-02-02 09:02:42 +0700

    add: triangle

--- src/main/python/anagrams.py
+++ src/main/python/anagrams.py
@@ -1,13 +1,22 @@
+import array
+
+# def Anagrams(word1,word2):
+#     if len(word1) != len(word2):
+#         return False
+# 
+#     charCount = array.array('i',(0 for i in range(0,26)))
+#     
+#     for i in len(word1):
+#         charCount[word1[i]-'a']
+
 
 def Anagrams(word1,word2):
-    if len(word1) != len(word2):
-        return False
 
-    charCount = [0 for i in range 26]
-    
-    for i in range(charCount):
-        print(charCount)
+    # sort word per character
+    if(sorted(word1) == sorted(word2)):
+        print("yes")
+    else:
+        print("no")
 
-
-Anagrams("a","a")
+Anagrams("listen","silent")
     
