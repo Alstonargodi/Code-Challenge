c0ad4ea3e68584f1814dc39b1e7b5b51585aee44
alston <wayanalston185@gmail.com>
2024-03-31 17:10:19 +0700

    add: exersicm kotlin / transcribeToRna

---
+++ src/main/kotlin/exercism/RNAtransiction.kt
@@ -0,0 +1,10 @@
+package exercism
+
+fun transcribeToRna(dna: String): String {
+    var transcribedRNA : String = ""
+    val RNAConversion : Map<Char,String> = mapOf('G' to "C",'C' to "G",'T' to "A",'A' to "U")
+
+    for(letter in dna) transcribedRNA += RNAConversion[letter]
+
+    return transcribedRNA
+}
