858814676b2c5630583184e43b304e2683914506
alston <wayanalston185@gmail.com>
2024-04-13 09:12:33 +0800

    add: exersicm kotlin / resister color

--- src/main/kotlin/exercism/ResistorColor.kt
+++ src/main/kotlin/exercism/ResistorColor.kt
@@ -1,11 +1,9 @@
 package exercism
 
 object ResistorColor {
-
     fun colorCode(input: String): Int {
         return colors().indexOf(input)
     }
-
     fun colors(): List<String> {
         return listOf(
             "black",
--- src/main/kotlin/exercism/ResistorDouColor.kt
+++
@@ -1,8 +0,0 @@
-import java.awt.Color
-
-object ResistorColorDuo {
-
-    fun value(vararg colors: Color): Int {
-      return colors[0].ordinal * 10 + colors[1].ordinal
-    }
-}
