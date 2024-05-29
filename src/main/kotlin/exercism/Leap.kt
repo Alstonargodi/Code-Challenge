cc4a16d194d8f26963751a2d21be3cc002f3225f
alston <wayanalston185@gmail.com>
2024-05-30 06:35:50 +0700

    add: exersicm kotlin / add leap (if expression)

--- src/main/kotlin/exercism/Leap.kt
+++ src/main/kotlin/exercism/Leap.kt
@@ -3,5 +3,9 @@ package exercism
 data class Year(val year: Int) {
 
     // TODO: Implement proper constructor
+    //never use in built function
     var isLeap: Boolean = year % 4 == 0 && year % 100 != 0 || year % 400 == 0
+
+    //if expresion
+    var isLeap2 : Boolean = if(year % 100 == 0) year % 400 == 0 else year % 4 == 0
 }
